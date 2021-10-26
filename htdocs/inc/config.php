087d196ee5ba3133f68ab7283f7ea95d282c88ce
Silvano Aldà <silvano.alda@sib.swiss>
2021-10-26 11:39:01 +0200

    🔧 extract languages from config.json to dedicated

--- htdocs/inc/config.php
+++ htdocs/inc/config.php
@@ -7,11 +7,19 @@ if (is_readable(__DIR__ . '/../conf/config.json')) {
         die('App config must be an object.');
     }
 
+    $languages_json = file_get_contents(__DIR__ . '/../conf/languages.json');
+    $languages = json_decode($languages_json);
+    if (!is_object($languages)) {
+        die('languages must be an object.');
+    }
+
+    $app_config->languages = $languages;
+
     // $_SERVER['SERVER_NAME'] = 'localhost';
     // Add server name in the cache path
     $app_config->cache->path = $app_config->cache->path . '/' . $_SERVER['SERVER_NAME'] . '/';
 
-// Dirty app config patch
+    // Dirty app config patch
     // $app_config->web_root = ($_SERVER['SERVER_NAME'] === 'localhost' ? '/htdocs/' : '/');
 } else {
     die('App config can\'t be loaded.');
