84bde2348f615457bdab1b05a34c636e6e1c06b5
jbarda <jonathan.barda@sib.swiss>
2022-05-18 17:39:31 +0200

    ⬆️ Added changes required for PHP 8.1.x

--- install.sh
+++ install.sh
@@ -5,13 +5,13 @@
 # Made by Jonathan Barda / SIB - 2020
 
 # Config
-PHP_VER="7.4"
+PHP_VER="8.1"
 
 # Install PPA
 sudo apt install -y software-properties-common
 sudo add-apt-repository ppa:ondrej/php
 
-# Install PHP 7.4 + extensions
+# Install PHP 8.1 + extensions
 sudo apt install -y \
     "php${PHP_VER}-cli" \
     "php${PHP_VER}-fpm" \
