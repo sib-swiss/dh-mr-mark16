2e5a2aabb824d1d68bcece41e2dee8c79340bcae
Silvano Aldà <silvano.alda@sib.swiss>
2022-08-16 17:37:33 +0200

    fix

--- htdocs/classes/Models/ManuscriptContentImage.php
+++ htdocs/classes/Models/ManuscriptContentImage.php
@@ -191,6 +191,10 @@ class ManuscriptContentImage extends ManuscriptContent
      */
     public function getCopyrightText()
     {
+        if(!$this->content){
+            return '';
+        }
+        
         $contentDecoded = json_decode($this->content);
         if (isset($contentDecoded->copyright)) {
             return $contentDecoded->copyright;
