6fa9da89243e0997ac6f8998ceac2bc0e9e8454d
Silvano Aldà <silvano.alda@sib.swiss>
2023-06-05 10:39:17 +0200

    #169: admin user

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,6 +46,6 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessFilament(): bool
     {
-        return str_ends_with($this->email, '@sib.swiss') && $this->hasVerifiedEmail();
+        return str_ends_with($this->email, '@sib.swiss'); // && $this->hasVerifiedEmail();
     }
 }
